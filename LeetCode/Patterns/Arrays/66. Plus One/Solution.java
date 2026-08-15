9c36cd4bffefe48e6340b683d281d09baa6cbcda
Adarsh Yadav <adarshkingqwert@gmail.com>
2026-08-15 23:53:06 +0530

    Add Java Solution to Patterns/Arrays

--- LeetCode/Patterns/Arrays/66. Plus One/Solution.java
+++ LeetCode/Patterns/Arrays/66. Plus One/Solution.java
@@ -2,16 +2,16 @@ class Solution {
     public int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] < 9) {
-                digits[i]++;    // just add 1 and return
+                digits[i]++;
                 return digits;
             }
-            digits[i] = 0;  // carry to next digit
+
+            digits[i] = 0;
         }
-        
-        // if all digits were 9 (like 999 -> 1000)
+
         int[] result = new int[digits.length + 1];
         result[0] = 1;
+
         return result;
-        
     }
 }
