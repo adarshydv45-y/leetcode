cf8447c57a48690e16cf641c02e474e4c8d70ca1
Adarsh Yadav <adarshkingqwert@gmail.com>
2026-08-17 22:04:48 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/1721. Swapping Nodes in a Linked List/Solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode t1 = head ;
+        ListNode t2 = head;
+        int n = 0;
+        while(t1 != null){
+            t1 = t1.next;
+            n++;
+        }
+        t1 = head;
+        for(int i = 0 ; i<n-k ; i++){
+            t1 = t1.next;
+        }
+        for(int j = 0 ; j<k-1 ; j++){
+            t2 = t2.next;
+        }
+        int temp = t1.val;
+        t1.val = t2.val;
+        t2.val = temp;
+
+        return head;
+        
+        
+        
+    }
+}
