043e50b79c540f68b94baa9d6afc576ae8af81c0
Adarsh Yadav <adarshkingqwert@gmail.com>
2026-08-19 12:25:03 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/34. Find First and Last Position of Element in Sorted Array/Solution.java
+++ LeetCode/Java/Medium/34. Find First and Last Position of Element in Sorted Array/Solution.java
@@ -1,3 +1,31 @@
+// class Solution {
+//     public int[] searchRange(int[] nums, int target) {
+//         int n = nums.length;
+//         int[] arr = {-1,-1};
+        
+//         for(int i = 0 ; i<n ; i++){
+//             if(nums[i]==target){
+//                 arr[0]=i;
+//                 break;
+            
+//             }
+//         }
+        
+//         for(int j = n-1 ; j>=0; j--){
+//             if(nums[n-1]==target){
+//                 arr[1]=j;
+//                 break;
+//             }
+            
+//         }
+//         return arr;
+
+        
+        
+        
+//     }
+// }
+
 
 
 class Solution {
