bce9a28b89a53fc050803060e07f7c2e4e3c8e8c
Adarsh Yadav <adarshkingqwert@gmail.com>
2026-08-19 21:07:57 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/528. Swapping Nodes in a Linked List/Solution.java
+++ LeetCode/Java/Medium/528. Swapping Nodes in a Linked List/Solution.java
@@ -10,26 +10,48 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        ListNode t1 = head ;
-        ListNode t2 = head;
-        int n = 0;
-        while(t1 != null){
-            t1 = t1.next;
-            n++;
-        }
-        t1 = head;
-        for(int i = 0 ; i<n-k ; i++){
-            t1 = t1.next;
-        }
-        for(int j = 0 ; j<k-1 ; j++){
-            t2 = t2.next;
-        }
-        int temp = t1.val;
-        t1.val = t2.val;
-        t2.val = temp;
+        // ListNode t1 = head ;
+        // ListNode t2 = head;
+        // int n = 0;
+        // while(t1 != null){
+        //     t1 = t1.next;
+        //     n++;
+        // }
+        // t1 = head;
+        // for(int i = 0 ; i<n-k ; i++){
+        //     t1 = t1.next;
+        // }
+        // for(int j = 0 ; j<k-1 ; j++){
+        //     t2 = t2.next;
+        // }
+        // int temp = t1.val;
+        // t1.val = t2.val;
+        // t2.val = temp;
 
+        // return head;
+
+
+
+
+
+        // ------Approach 2 -------
+        ListNode fast  = head;
+        ListNode slow = head ;
+        for(int i = 1 ; i<= k ; i++){
+            fast = fast.next;
+        }
+        while(fast != null){
+            slow = slow.next;
+            fast= fast.next;
+        }
+        fast = head;
+        for(int i = 1 ; i<=k-1 ; i++){
+            fast = fast.next ;
+        }
+        int temp = fast.val ;
+        fast.val = slow.val;
+        slow.val = temp;
         return head;
-        
         
         
     }
