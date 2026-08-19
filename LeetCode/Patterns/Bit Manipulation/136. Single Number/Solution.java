7e7277939e04e87bfc0a27c9b966601a355f4287
Adarsh Yadav <adarshkingqwert@gmail.com>
2026-08-19 22:53:25 +0530

    Add Java Solution to Patterns/Bit Manipulation

--- LeetCode/Patterns/Bit Manipulation/136. Single Number/Solution.java
+++ LeetCode/Patterns/Bit Manipulation/136. Single Number/Solution.java
@@ -2,36 +2,43 @@ class Solution {
     public int singleNumber(int[] nums) {
 
         // ----------Approach 1-----------
-
         // int n = nums.length;
-
         // for (int i = 0; i < n; i++) {
-
         //     int count = 0;
-
         //     for (int j = 0; j < n; j++) {
-
         //         if (nums[i] == nums[j]) {
         //             count++;
         //         }
         //     }
-
         //     if (count == 1) {
         //         return nums[i];
         //     }
         // }
-
         // return -1;
 
 
         // ------------Approach 2 -----------
+        // int sing = nums[0] ;
+        // int n = nums.length;
+        // for(int i = 1; i<n ; i++){
+        //     sing = sing^nums[i];
+        // }
+        // return sing;
 
-        int sing = nums[0] ;
-        int n = nums.length;
-        for(int i = 1; i<n ; i++){
-            sing = sing^nums[i];
+
+
+
+        // --------Approch 3 --------
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
-        return sing;
+        for(int num : nums){
+            if(map.get(num)==1){
+                return num;
+            }
+        }
+        return -1;
 
     }
 }
